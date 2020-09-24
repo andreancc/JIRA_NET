b4ff5e38e3f6e59b7fd9c422d05d1f8edccaba6a
andreancc <andreancc@hotmail.com>
2020-09-24 15:39:54 -0500

    last commit

---
+++ NETChallenge/PageObjects/BrowserFactory.cs
@@ -0,0 +1,33 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Firefox;
+
+
+
+
+namespace NETChallenge.PageObject
+{
+    class BrowserFactory
+    {
+        private static IWebDriver driver;
+        public static IWebDriver InitiatetBrowser(string browserName)
+        {
+            switch (browserName)
+            {
+                case "Firefox":
+                driver = new FirefoxDriver();
+                    break;
+
+                case "Chrome":
+
+                    driver = new ChromeDriver();
+                    break;
+              
+            }
+            return driver;
+        }
+
+
+
+    }
+}
